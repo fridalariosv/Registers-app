69178f9ec2dc02c088a142867e52829885905e3a
fridalariosv <fridasaraiv@gmail.com>
2022-02-07 11:29:28 -0600

    updates in the details code

--- src/app/pages/details-products/details-products.page.ts
+++ src/app/pages/details-products/details-products.page.ts
@@ -1,4 +1,3 @@
-import { Product } from './../products/interfaces/product.interface';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../products/services/products.service';
@@ -9,7 +8,6 @@ import { ProductsService } from '../products/services/products.service';
   styleUrls: ['./details-products.page.scss'],
 })
 export class DetailsProductsPage implements OnInit {
-  products : Product[];
   idProduct: number;
   product: any;
   
@@ -18,42 +16,13 @@ export class DetailsProductsPage implements OnInit {
     private route: ActivatedRoute
   ) { }
 
-  setData(data): void{
-    this.product = data;
-    console.log(this.product); // setear la data a product
-  }
-
   ngOnInit() {
-    //this.productSvc.getProducts().pipe(tap((x: Product[]) => ( this.products = x))).subscribe();
-    //console.log(this.products, 'desde detalles');
-    //const url = `http://localhost:3000/products`
-
     const routeParams = this.route.snapshot.paramMap;
-    this.idProduct = Number(routeParams.get('id')); // al asignar id al get, es la variable que le pasa desde el html
-
-    /*
+    this.idProduct = Number(routeParams.get('id'));
     this.productSvc
     .getProduct(this.idProduct)
-    .pipe(tap(( product: Product[]) => (this.product = product, console.log(product, 'producto'))))
-    .subscribe();
-    */
-    this.productSvc
-    .getProduct(this.idProduct)
-    .subscribe(
-      (response) => {
-        this.setData(response)
-      }
-    )
+    .subscribe(response => {
+      this.product = response;
+    })
   }
-
-  dataTest = { name: 'james'}
 }
-
-/*
-    const routeParams = this.route.snapshot.paramMap;
-    this.idCostumer = Number(routeParams.get('idCustomer'));
-    this.idDocument = String(routeParams.get('idDocument'));
-    const dataDocuments = await this.service.HttpResponse('getDocumentDetails', { code_register: this.idDocument })
-    if (dataDocuments.status == 'Success') this.document = dataDocuments.data[0]
-
-  */
